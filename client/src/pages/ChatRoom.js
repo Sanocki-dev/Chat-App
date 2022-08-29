e255604a2fd43d1c47c2139f4484f69f358b49a5
sanocki <sanockimike@gmail.com>
2022-08-29 13:59:26 -0400

    Added avatars + fixed crashing

--- client/src/pages/ChatRoom.js
+++ client/src/pages/ChatRoom.js
@@ -11,7 +11,7 @@ function ChatRoom() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const newSocket = io(`http://${window.location.hostname}:8000`);
+    const newSocket = io(`/`);
     setSocket(newSocket);
 
     // Tries to connect to the room using the room and username
